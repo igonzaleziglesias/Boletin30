436372f35ad1fced4b47ba2fbb4939aad7c87282
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-04-30 12:10:34 +0200

    boletin30

--- boletin30/Boletin30.java
+++ boletin30/Boletin30.java
@@ -1,9 +1,27 @@
 package boletin30;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class Boletin30 {
 
     public static void main(String[] args) {
-
+        ArrayList<SeleccionFutbol> selec = new ArrayList<>();
+        SeleccionFutbol futbolista = new Futbolista(3, "delantero", 1, 20, "carlos", "Blanco");
+        SeleccionFutbol entrenador = new Entrenador(1, 2, 38, "antonio", "Cojera Maxima");
+        SeleccionFutbol masajista = new Masajista("fisioterapeuta", 6, 3, 32, "Sara", "Rodriguez Martinez");
+        SeleccionFutbol seleccionador = new Seleccionador(4, 40, "Diego", "Perez LLorente");
+        futbolista.concentrarse();
+        entrenador.concentrarse();
+        masajista.concentrarse();
+        seleccionador.concentrarse();
+        selec.add(futbolista);
+        selec.add(entrenador);
+        selec.add(masajista);
+        selec.add(seleccionador);
+        for (SeleccionFutbol f : selec) {
+            JOptionPane.showMessageDialog(null, f);
+        }
     }
 
 }
