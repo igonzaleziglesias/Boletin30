5a7706aaba12254f8144da23e3aa6b6b2cc6db30
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-04-30 12:05:49 +0200

    boletin30

---
+++ boletin30/Boletin30.java
@@ -0,0 +1,9 @@
+package boletin30;
+
+public class Boletin30 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ boletin30/IntegranteSeleccionFutbol.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin30;
+
+/**
+ *
+ * @author igonzaleziglesias
+ */
+public interface IntegranteSeleccionFutbol {
+    public void concentrarse();
+    public void viajar();
+    public void entrenar();
+    public void jugarPartido();
+}
